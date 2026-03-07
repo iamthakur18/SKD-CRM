b13c370bf53be8630e7211fa1884c438b1d1d012
iamthakur18 <77274491+iamthakur18@users.noreply.github.com>
2026-03-07 15:38:22 +0530

    Add files via upload
    MAKE IT MOBILE FRIENDLY

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'skd-crm-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['/', '/index.html']).catch(()=>{}))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match('/index.html'))
+    );
+  }
+});
